10c8d1ba611863f4bc594d031dd8dc50958f7cf0
Arthur Rio <arthur@punchtab.com>
2015-06-18 01:07:35 -0700

    first commit

---
+++ manytomanybug/app3/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app2', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FormA',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.TextField()),
+                ('signature', models.ManyToManyField(related_name='+', to='app2.Upload')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='FormB',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.TextField()),
+                ('signature', models.ManyToManyField(related_name='+', to='app2.Upload')),
+            ],
+        ),
+    ]
