c5fef49fac370033052bf15c5c43fc5e50ae2668
Arthur Rio <arthur@punchtab.com>
2015-06-18 02:23:38 -0700

    switch back to sqllite for making the tests easier tand faster to run

--- manytomanybug/settings.py
+++ manytomanybug/settings.py
@@ -79,9 +79,8 @@ WSGI_APPLICATION = 'manytomanybug.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'manytomanybug',
-        'USER': 'root',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
